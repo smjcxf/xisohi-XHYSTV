b143f4e6a4311db78ce42ae4e4a9a4e546ccbab8
xihi <xuhui0607@gmail.com>
2024-09-25 22:44:25 +0800

    Update Constants.java

--- app/src/main/java/com/github/tvbox/osc/ui/xupdate/Constants.java
+++ app/src/main/java/com/github/tvbox/osc/ui/xupdate/Constants.java
@@ -23,7 +23,7 @@ package com.github.tvbox.osc.ui.xupdate;
  */
 public final class Constants {
 
-    public static final String UPDATE_DEFAULT_URL = "https://xhys.lcjly.cn/XHYSTVupdate.json";
+    public static final String UPDATE_DEFAULT_URL = "https://xhys.lcjly.cn/update/XHYSTV.json";
 
     // 更新服务域名
     public static final String UPDATE_URL = "http://xxx.xxx";
